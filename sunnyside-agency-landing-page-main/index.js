7bf9570fa15c9dcb1b82e6d6d98e849d15c9291d
emmmanuel <80278315+LeviOghene@users.noreply.github.com>
2024-01-20 11:54:30 +0100

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/index.js
@@ -0,0 +1,7 @@
+var menuButton = document.getElementById('menuButton');
+
+menuButton.addEventListener('click', function () {
+    var menu = document.querySelector('.mobile-menu');
+    
+    menu.style.display = 'block';
+})
